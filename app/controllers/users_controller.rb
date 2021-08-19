66ba293da9fbfd31425f4e408c15056642ebbfca
charcodes24 <charcodes24@gmail.com>
2021-08-19 13:47:15 -0700

    commit tree d925b3831c5e68aa27f4672bae35c7a8a7761552

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def show
+    one_user = User.find(params[:id])
+    render json: one_user, include: :items
+  end
 end
